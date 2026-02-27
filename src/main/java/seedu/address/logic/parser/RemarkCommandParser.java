315bd6c259befe0be7c69a2994d8689366491347
Tan Je-Deon <114093710+momentumnn@users.noreply.github.com>
2026-02-27 14:54:17 +0800

    Add Remark into RemarkCommand
    Update private variables of value within RemarkCommand from
    String to Remark.
    Change instances of String to Remark on RemarkCommandParser
    Include RemarkTest
    Update AddressBookParserTest, RemarkCommandTest, RemarkCommandParserTest
    to use Remark instead of String.

--- src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
+++ src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
@@ -7,10 +7,14 @@ import seedu.address.commons.core.index.Index;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.RemarkCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.person.Remark;
 
+/**
+ * Parses input arguments and creates a new RemarkCommand object
+ */
 public class RemarkCommandParser implements Parser<RemarkCommand> {
 
-    public static final Prefix PREFIX_REMARK = new Prefix("\r");
+    public static final Prefix PREFIX_REMARK = new Prefix("/r");
 
     @Override
     public RemarkCommand parse(String args) throws ParseException {
@@ -28,6 +32,6 @@ public class RemarkCommandParser implements Parser<RemarkCommand> {
 
         String remark = argMultimap.getValue(PREFIX_REMARK).orElse("");
 
-        return new RemarkCommand(index, remark);
+        return new RemarkCommand(index, new Remark(remark));
     }
 }
